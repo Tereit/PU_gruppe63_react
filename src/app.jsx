5ea24423f43893723f7f50da54d51cf2a184545c
Morten Mulvik <morten_mulvik@hotmail.com>
2017-03-11 16:01:32 +0100

    updated .gitignore to skip build folder added login.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import Login from './login.jsx';
 
 class App extends Component {
   render() {
