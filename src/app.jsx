f262026ef42657b0720632c4c2c592baf49de556
Morten Mulvik <mortenmu@stud.ntnu.no>
2017-03-13 11:00:16 +0100

    now shows login page if user isn't logged in, after logging inn the lecture page is shown

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Login from './login.jsx';
+import Lecture from './lecture.jsx';
+
+const firebase = require('./firebaseApp.js').firebase;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false
+    }
+    firebase.auth().onAuthStateChanged(user => {
+    	if(user){
+        console.log("signed inn");
+        this.setState({
+          loggedIn: true
+        });
+      }
+      if(!user) {
+        console.log("signed out");
+        this.setState({
+          loggedIn: false
+        });
+      }
+    });
+  }
   render() {
     return (
       <div>
-        <Login />
+        {this.state.loggedIn ? <Lecture /> : <Login />}
       </div>
     );
   }
