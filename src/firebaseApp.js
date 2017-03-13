4a5129a8b3d7cf3302465b760d5e2308bcc7211b
Morten Mulvik <morten_mulvik@hotmail.com>
2017-03-13 09:27:07 +0100

    created a global firebase scope to use auth: var auth = require('./firebaseApp.js').auth; to use database: var database = require('./firebaseApp.js').database;

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,13 +1,17 @@
 import firebase from 'firebase';
 
 var config = {
-  apiKey : "AIzaSyA3wCVMG1f33-WnafKkgF6cNtnoDFJB_IM",
-  authDomain : "lecturify.firebaseapp.com",
-  databaseURL : "https://lecturify.firebaseio.com",
-  storageBucket : "lecturify.appspot.com",
-  messagingSenderId : "946578632705"
+    apiKey: "AIzaSyA3wCVMG1f33-WnafKkgF6cNtnoDFJB_IM",
+    authDomain: "lecturify.firebaseapp.com",
+    databaseURL: "https://lecturify.firebaseio.com",
+    storageBucket: "lecturify.appspot.com",
+    messagingSenderId: "946578632705"
 };
-var fbApp = firebase.initializeApp(config);
+var fb = firebase.initializeApp(config);
+console.log(fb.auth());
 
-module.exports.fbAuth = fbApp.auth();
-module.exports.fbData = fbApp.database();
+var auth = fb.auth();
+var database = fb.database();
+
+exports.auth = auth;
+exports.database = database;
