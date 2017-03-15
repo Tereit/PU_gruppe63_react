849a7d048ec26d480893d3e9136fc3f912b9b520
Kjetil Vaagen <kjetivaa@stud.ntnu.no>
2017-03-15 10:49:17 +0100

    professor side

--- src/professor.jsx
+++ src/professor.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
-
 export default class Professor extends Component {
 
   render() {
     return (
-      <div>
-        <p>Professor</p>
+      <div style={styles.container}>
+        <nav style={navbar}>
+					<span>Professor</span>
+				</nav>
+				<div style={centeredContainer}>
+					<p>Professor</p>
+				</div>
       </div>
     );
   }
+}
+
+const styles = {
+	container: {
+		flex: 1,
+	},
+	navbar: {
+		flexDirection: "row",
+		alignItems: "stretch",
+		justifyContent: "center",
+		backgroundColor: "grey",
+	},
+	centeredContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "black",
+	}
 }
